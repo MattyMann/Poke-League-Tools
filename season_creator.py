73e0e20fc3e36c06ff12aae1febadd7c41888fc6
Clos3y <sam.w.close@gmail.com>
2023-02-12 19:21:32 +0000

    Working season creator

--- season_creator.py
+++ season_creator.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import math
 import numpy as np
+from itertools import combinations
 
 def conventions( rankings ) -> list:
 
@@ -9,8 +10,10 @@ def conventions( rankings ) -> list:
     
     ratings = ratings.astype({'username': 'string', 'elo': 'int16', 'active': 'bool'}).sort_values('elo',ascending=False)
     
+    # Create a new DF with just the active players
     active_players = ratings[ratings['active'] == True].drop(columns=['elo','active'])
     
+    # How many conventions should be made? By default we say there should be eight per convention
     num_of_conventions = math.ceil(len(active_players) / 8)
     
     active_players = active_players.to_numpy()
@@ -22,3 +25,50 @@ def conventions( rankings ) -> list:
         conventions[idx] = list(map(lambda player: player[0],convention))
         
     return conventions
+
+def matches( convention: list):
+
+    num_players = len(convention)
+
+    if num_players % 2:
+        convention.append('Day Off')
+        num_players += 1
+
+    match_list = list(combinations(convention,2))
+
+    num_days = num_players - 1
+
+    games_per_day = num_players // 2
+
+    schedule = np.empty((num_days,games_per_day,2),dtype='object')
+
+    while match_list != []:
+
+        for nday, day in enumerate(schedule):
+            
+            if match_list == []:
+                break
+            
+            else:
+                t_val = sum(map(lambda player: True if player in day else False, match_list[-1]))
+
+                if t_val != 0:
+
+                    pass 
+
+                else:
+
+                    for ngame, game in enumerate(day):
+
+                        if game.all() == None:
+                        
+                            schedule[nday,ngame] = match_list.pop()
+
+                            break
+
+    print(schedule)
+
+conv = conventions("data/rankings.pq")
+
+for i in conv:
+    matches(i)
